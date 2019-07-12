2cd959bc21a959df85ba15d2bb0ae0f54196d13d
Javier Alba <fjavieralba@gmail.com>
2019-07-12 13:40:14 +0200

    double linked list implementation and tests

--- linked_list/double_linked_list.py
+++ linked_list/double_linked_list.py
@@ -1,12 +1,53 @@
 class Node():
-    def __init__(self, data, previous=None, next=None):
+    def __init__(self, data, prev=None, next=None):
         self.data = data
-        self.previous = previous
+        self.prev = prev
         self.next = next
         
 class DoubleLinkedList:
-    def __init__(self):
+    def __init__(self, head=None, last=None):
         self.head = None
+        self.last = None
 
-    def isEmpty(self):
-        return self.head == None    
+    def is_empty(self):
+        return self.head == None
+    
+    def append(self, node):
+        """ appends a node at the end of the list """
+        if self.is_empty():
+            self.head = node
+            self.last = node
+            node.next = None
+            node.prev = None
+        else:
+            self.last.prev = node
+            node.next = self.last
+            self.last = node
+    
+    def prepend(self, node):
+        """ adds a node to the beginning of the list """
+        if self.is_empty():
+            self.head = node
+            self.last = node
+            node.next = None
+            node.prev = None
+        else:
+            self.head.next = node
+            node.prev = self.head
+            self.head = node
+    
+    def get_at_index(self, index):
+        if self.is_empty():
+            raise RuntimeError("Empty list")
+        current_index = 0
+        current_node = self.head
+        while current_node is not self.last:
+            if index == current_index:
+                return current_node
+            else:
+                current_index += 1
+                current_node = current_node.prev
+        if current_index == index:
+            return current_node
+        else:
+            raise RuntimeError("Out of Bounds")
--- linked_list/tests/double_linked_list_tests.py
+++ linked_list/tests/double_linked_list_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from double_linked_list import DoubleLinkedList
+from double_linked_list import DoubleLinkedList, Node
 
 class TestDoubleLinkedList(unittest.TestCase):
 
@@ -9,4 +9,42 @@ class TestDoubleLinkedList(unittest.TestCase):
     def test_init_with_no_args(self):
         """ creates an empty list by default """
         l = DoubleLinkedList()
-        self.assertTrue(l.isEmpty())
+        self.assertTrue(l.is_empty())
+    
+    def test_append_single_elem(self):
+        l = DoubleLinkedList()
+        n = Node('data')
+        l.append(n)
+        self.assertFalse(l.is_empty())
+        self.assertEqual(l.head, n)
+    
+    def test_append_then_prepend(self):
+        l = DoubleLinkedList()
+        n1 = Node(1)
+        n2 = Node(2)
+        l.append(n1)
+        l.prepend(n2)
+        self.assertEqual(l.head, n2)
+    
+    def test_get_on_empty_list(self):
+        l = DoubleLinkedList()
+        with self.assertRaises(RuntimeError):
+            l.get_at_index(5)
+    
+    def test_get_at_index(self):
+        l = DoubleLinkedList()
+        n1 = Node(1)
+        n2 = Node(2)
+        n3 = Node(3)
+        l.append(n1)
+        l.prepend(n2)
+        l.append(n3)
+        self.assertEqual(l.get_at_index(0), n2)
+        self.assertEqual(l.get_at_index(1), n1)
+        self.assertEqual(l.get_at_index(2), n3)
+    
+    def test_get_at_index_out_of_bounds(self):
+        l = DoubleLinkedList()
+        l.append(Node(1))
+        with self.assertRaises(RuntimeError):
+            l.get_at_index(5)
